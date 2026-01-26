25cbbbf3e00a624e4e074334f50083ff27a6dca7
Person <localboxcrox@gmail.com>
2026-01-25 21:42:37 -0500

    update dep eta-core

--- crates/cli/examples/lambda.lisp
+++ crates/cli/examples/lambda.lisp
@@ -1,20 +1,31 @@
-((E ((A A) E)) ;main
-;Lx
-(E (
-(P (E
-;Px
-;lambda calculus expression here
-;note: P is application, A is abstraction as we have set up here
-(P ((A (x (x x))) x))
-;/lambda
-;/Px
-))
-;Pi
-(
+;a comment starts with ; and continues to end of line
+((E ((A A) E)) ;external capture of eta and omicron
+
+(E (; invoking eta
+(P ;capturing lambda application implementation from below
+;<---lambda expression begin--->
+
+;NOTE: as we have described,
+;   application is of form (E (P (<lambda abstraction>  <applicand>)))
+;   abstraction is of form (A (<binding variabel> <body>))
+;NOTE: alpha renaming is not a real thing, make sure to name accordingly
+
+;;;;;;;omega combinator;;;;;;;;
+;uncomment the following to make your stack overflow ☺️
+;; (E (P (
+;; (A (y (E (P (y y)))))
+;; (A (y (E (P (y y)))))
+;; )))
+
+; make 4 of the virus 🦠
+(E(P ((A (v ((v v) (v v)))) 🦠)))
+
+;<---lambda expression end--->
+)
+
+(;this is the lambda application implementation to be used as P
 ((A (n b)) x)
 (E ((n b) x))
 )
-;/Pi
-))
-;/Lx
-) ;/main
+
+)))
